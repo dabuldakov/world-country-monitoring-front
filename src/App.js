dd332c82cd0fcd2773c894797a82d86e9496cd1b
Булдаков Дмитрий Александрович <dmitriy.buldakov@rosbank.ru>
2025-04-28 21:25:37 +0700

    add charts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import BarColumn from "./chart/BarColumn";
+import SimpleLine from "./chart/SimpleLine";
 
-function App() {
+export default function App() {
+  const [data, setdata] = useState();
+
+  const filterData = (data) => {
+    return data
+    .map(({ Year, Population }) => ({ Year, Population }))
+    .sort((a, b) => a.Year - b.Year);
+  };
+
+  useEffect(() => {
+    const fetchDatas = async () => {
+      const res = await fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population");
+      const data = await res.json();
+      console.log(data);
+      const filteredData = filterData(data?.data || []);
+      console.log(filteredData);
+      setdata(filteredData);
+    };
+    fetchDatas();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">How to use Recharts with React
+      <BarColumn data={data} />
+      <SimpleLine data={data} />
     </div>
   );
 }
-
-export default App;
