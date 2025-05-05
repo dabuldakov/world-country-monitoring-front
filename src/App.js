db660e92fe78aaffa0e8331e89a097c761685223
Булдаков Дмитрий Александрович <dmitriy.buldakov@rosbank.ru>
2025-05-05 17:02:19 +0700

    country

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import SimpleLine from "./chart/SimpleLine";
 
 export default function App() {
   const [data, setdata] = useState();
+  const [countries, setCountries] = useState(["RUS", "USA", "CHN", "IND"]);
+  const [selectedCountry, setSelectedCountry] = useState("RUS");
 
   const filterData = (data) => {
     return data
@@ -11,20 +13,32 @@ export default function App() {
   };
 
   const baseUrl = process.env.API_URL || 'http://localhost:8080';
+  const countryApi = 'api/wcm/v0/international-reserve/country';
 
   useEffect(() => {
     const fetchDatas = async () => {
-      const res = await fetch(`${baseUrl}/api/wcm/v0/international-reserve/country/RUS`);
+      const res = await fetch(`${baseUrl}/${countryApi}/${selectedCountry}`);
       const data = await res.json();
       const filteredData = filterData(data || []);
       setdata(filteredData);
     };
     fetchDatas();
-  }, []);
+  }, [selectedCountry]);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
 
   return (
     <div className="App">Country Monitoring
-      <BarColumn data={data} />
+      <select value={selectedCountry} onChange={handleCountryChange}>
+        {countries.map((country) => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
       <SimpleLine data={data} />
     </div>
   );
